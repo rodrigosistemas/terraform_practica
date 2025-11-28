ef1867f2e1583c075d49610378e6af43d6befc84
rodrigosistemas <rodhernandez.code@gmail.com>
2025-11-28 14:36:50 -0500

    uso de variable en archivo externo

--- main.tf
+++ main.tf
@@ -14,11 +14,6 @@ provider "azurerm" {
   subscription_id = "c7b5fe30-7539-43a0-be8b-fb6ae63b5c76"
 }
 
-variable "rg_name" {
-  type = string
-  default = "resource-group"
-}
-
 resource "azurerm_resource_group" "rg" {
   location = "centralus"
   name = var.rg_name
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "rg_name" {
+  type = string
+  default = "resource-group"
+}
